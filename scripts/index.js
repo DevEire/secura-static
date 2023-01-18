2ef9a37d23bb375c795b36915db234b4459e80f9
Darragh <darragh@deveire.com>
2023-01-18 21:35:39 +0000

    Add static files

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+$(function() {
+  $(".search-opener").click(function() {
+    $(".search-bar").slideDown();
+    $(".search-bar input[type='text']").focus();
+  });
+
+  $(".close-search").click(function() {
+    $(".search-bar").animate({ height: "toggle", opacity: "toggle" }, "slow");
+  });
+
+  $(".dropdown").hover(function() {
+    $(".dark-background").toggle();
+  });
+
+  $(".left-col").click(function() {
+    $(".side-menu").css("left", "0");
+    $(".dark-background").addClass("dark-background-mobile");
+  });
+
+  $(".dark-background, .side-menu-close").click(function() {
+    $(".side-menu").css("left", "-100%");
+    $(".dark-background").removeClass("dark-background-mobile");
+  });
+});
